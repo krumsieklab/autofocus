e773a4b649e770a220a4e1c904032f7c127a7106
aschweickart <aas4002@med.cornell.edu>
2020-10-12 12:31:08 -0400

    Make testing flexible for cluster

--- AF_testing.R
+++ AF_testing.R
@@ -23,23 +23,39 @@ score_method = "lm"
 adjust_method = "holm"
 #adjust_method = "wy"
 
+cores = 6
+
 
 QD <- initialize(t(assay(all_platforms)), 
                    sample.data = colData(all_platforms),
                    mol.data = rowData(all_platforms))
 
-QD_results <- find_sig_clusts(QD, 
+QD_results_lm <- find_sig_clusts(QD, 
                               "DIAB", c("AGE", "SEX", "BMI"), 
-                              score_method = score_method, 
+                              score_method = "lm", 
                               adjust_method = adjust_method, 
-                              cores = 6)
+                              cores = cores)
+
+QD_results_pc <- find_sig_clusts(QD, 
+                                 "DIAB", c("AGE", "SEX", "BMI"), 
+                                 score_method = "pc", 
+                                 adjust_method = adjust_method, 
+                                 cores = cores)
 
 AD <- initialize(t(assay(ADNI_platforms)), 
                  sample.data = colData(ADNI_platforms),
                  mol.data = rowData(ADNI_platforms))
 
+AD_results_lm <- find_sig_clusts(AD, 
+                              "ADAS.Cog13", c("Age", "Sex", "Education"), 
+                              score_method = "lm", 
+                              adjust_method = adjust_method,
+                              cores = cores)
+
+
 AD_results <- find_sig_clusts(AD, 
                               "ADAS.Cog13", c("Age", "Sex", "Education"), 
-                              score_method = score_method, 
+                              score_method = "pc", 
                               adjust_method = adjust_method,
-                              cores = 6)
+                              cores = cores)
+
