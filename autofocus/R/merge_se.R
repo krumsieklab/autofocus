27c5dcdf339211094c018e28d7b6a27503f18eec
KelseyChetnik <kelsey.chetnik@gmail.com>
2023-07-17 17:04:06 -0400

    bug fix: merge_se
    Fixed a bug in merge_se. Function was not combining sample columns in assay correctly.

--- autofocus/R/merge_se.R
+++ autofocus/R/merge_se.R
@@ -33,14 +33,25 @@
 #'
 #' @author KC
 #'
+#' @importFrom SummarizedExperiment rowData
+#' @importFrom SummarizedExperiment colData
+#' @importFrom SummarizedExperiment assay
+#'
 #' @export
 merge_se <- function(D_list, samp_id, feat_id, phenos, confounders){
 
-  # TO DO: Add checks to validate arguments
+  # Check that samp_id and assay columns match; throw warning if not
+  sample_list <- sapply(D_list, function(x){colData(x)[[samp_id]]}) %>% unlist()
+  assay_cols <- sapply(D_list, function(x){colnames(assay(x))}) %>% unlist()
+  if(!identical(sample_list, assay_cols)) warning("Assay column names do not match sample IDs. User must ensure correct ordering of assay columns.")
 
   # check features are unique
   feature_list <- sapply(D_list, function(x){rowData(x)[[feat_id]]}) %>% unlist()
   if(any(duplicated(feature_list))) stop("Features must be unique!")
+
+  # Check that feat_id and assay rows match; throw warning if not
+  assay_rows <- sapply(D_list, function(x){rownames(assay(x))}) %>% unlist()
+  if(!identical(feature_list, assay_rows)) warning("Assay row names do not match feature IDs. User must ensure correct ordering of assay rows.")
 
   # check required colData columns are present
   req_col <- c(samp_id, phenos, confounders)
@@ -48,7 +59,13 @@ merge_se <- function(D_list, samp_id, feat_id, phenos, confounders){
   if(!any(check_cols)) stop("Phenotype and / or confounder columns missing in one or more SEs.")
 
   # get combined assay
-  df_list <- lapply(D_list, function(x){assay(x) %>% t() %>% as.data.frame() %>% tibble::rownames_to_column(samp_id)})
+  df_list <- lapply(D_list, function(x){
+    df <- assay(x) %>% as.data.frame()
+    colnames(df) <- colData(x)[[samp_id]]
+    rownames(df) <- rowData(x)[[feat_id]]
+    df <- df %>% t() %>% as.data.frame() %>% tibble::rownames_to_column(samp_id)
+    df
+  })
   full_df <- df_list %>% purrr::reduce(dplyr::full_join, by=samp_id) %>% tibble::column_to_rownames(samp_id) %>%
     t() %>% as.data.frame()
 
