10db9e064024add2567432c9a49aea6640683b17
aschweickart <aschweickart@hmc.edu>
2024-03-07 13:00:21 -0500

    Add two phenotypes to output cluster table

--- autofocus/R/output_cluster_table.R
+++ autofocus/R/output_cluster_table.R
@@ -4,7 +4,7 @@
 #'
 #' @param R R_struct object output by initialize_R
 #' @param threshold Enrichment threshold at which to calculate cluster enrichment
-#' @param phenotype Which phenotype associated clusters to look at. Default "both" to see all phenotypes' clusters
+#' @param phenotype Which phenotype associated clusters to look at. Default "all" to see all phenotypes' clusters
 #' @param name Column name in R$annos specifying the node name
 #' @param annotations_to_show Vector of colnames of R$annos to return with nodes
 #'
@@ -15,9 +15,35 @@
 #'
 #'
 #' @export
-output_cluster_table <- function(R, threshold, phenotype = "both", name, annotations_to_show){
+output_cluster_table <- function(R, threshold, phenotype = "all", name, annotations_to_show){
+  # Get enrichment peaks at input threshold
   peaks =  peak_finder_wrapper(R, threshold = threshold)
-  clusts_to_show <- R$clust_info[(peaks!="#666666"&R$clust_info$Size>1 ),] # Get the clusters that are peaks and not leaves
+
+  # If we only have one phenotype, show all peaks
+  if(length(R$phenotypes) == 1){
+    clusts_to_show <- R$clust_info[(peaks!="#666666"&R$clust_info$Size>1 ),] # Get the clusters that are peaks and not leaves
+    p_cols <- c("p_value")
+  }
+
+  # If two phenotypes, determine which to show
+  else{
+    pheno1_colors<-c('#E7298A', "#FC8D62")
+    pheno2_colors<-c("#A6D854","#FC8D62")
+
+    if (phenotype == R$phenotypes[1]){
+      clusts_to_show <- R$clust_info[(peaks%in%pheno1_colors&R$clust_info$Size>1 ),] # Get the clusters that are peaks and not leaves
+      p_cols <- c("pheno1_p_value")
+    }
+    else if (phenotype == R$phenotypes[2]){
+      clusts_to_show <- R$clust_info[(peaks%in%pheno2_colors&R$clust_info$Size>1 ),] # Get the clusters that are peaks and not leaves
+      p_cols <- c("pheno2_p_value")
+    }
+    else{
+      clusts_to_show <- R$clust_info[(peaks!="#666666"&R$clust_info$Size>1 ),] # Get the clusters that are peaks and not leaves
+      p_cols <- c("pheno1_p_value","pheno2_p_value")
+    }
+  }
+
   nodes_to_show <- R$clust_info
 
   # this subselects the annos for the clusters of interest (from R$annos)
@@ -25,7 +51,7 @@ output_cluster_table <- function(R, threshold, phenotype = "both", name, annotat
   lapply(1:nrow(clusts_to_show), function(i){
     annos <- R$annos[R$clusts[[clusts_to_show$ClusterID[i]]],] # Get annotations for molecules in the peak clusters
     annos$cluster =rep(clusts_to_show$ClusterID[i], nrow(annos)) # Make a new column that just reps the cluster id for each molecule
-    data.frame(annos[,c(name, annotations_to_show, "p_value","cluster")])
+    data.frame(annos[,c(name, annotations_to_show, p_cols,"cluster")])
 
   }) %>% bind_rows()
 
